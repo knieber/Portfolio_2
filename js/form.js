e107a5d9785a798c2a8aaf6989f3b8517f405a56
knieber <kyle.nieber@hotmail.com>
2015-04-27 18:08:09 -0400

    Update 4-27-15

---
+++ js/form.js
@@ -0,0 +1,41 @@
+//Problem: Form does not appear when CONTACT ME or CONTACT is clicked
+//Solution: Make Form appear when CONTACT ME or CONTACT is clicked
+
+var $overlay = $('<div id="overlay"></div>');
+var $form = $('.contact-form');
+
+
+$overlay.append($form);
+
+$("body").append($overlay);
+
+function contactButton(event) {
+  event.preventDefault();
+
+  //Show the overlay.
+  $overlay.fadeIn(400, function() {
+    $overlay.show();
+  });
+  //Show Form
+  $form.fadeIn(400, function() {
+    $form.show();
+  });
+
+}
+
+//Capture the click event on a link to an image
+$(".contact-button button").click(contactButton);
+
+$(".contact").click(contactButton);
+
+
+//When overlay is clicked
+$overlay.click(function(){
+  //Hide the overlay
+  $overlay.fadeOut(400, function() {
+    $overlay.hide();
+  });
+  $form.fadeOut(400, function() {
+    $form.hide();
+  });
+});
