8e60114eabc476fbf5043abad052947e3b7d6706
knieber <kyle.nieber@hotmail.com>
2015-05-01 11:36:15 -0400

    Updated Mobile Nav

--- js/mobileNavOld.js
+++ js/mobileNavOld.js
@@ -1,35 +1,38 @@
 //Problem: Fixed nav always displays extended, blocking content
 //Solution: Provide a way to toggle nav to display links
 
+var homeli = ('<a id="tempHome" href="index.html"><li>HOME</li></a>');
+var contactli = ('<a id="tempContact" href="#" class="contact"><li>CONTACT</li></a>');
 
-var $menuList = $(".mobileListMenu ul");
-var $mobileNav = $("#mobileNav");
 
-
-$(".topNavButton").click(function(){
+$(".mobileDiv").click(function(){
   var clicks = $(this).data('clicks');
 
   if(clicks) {
     //Even click
 
     //Bring Menu back to original state
-    $mobileNav.animate({
+    $(".nav").animate({
       height: "60px"
     }, 400);
 
     //hide .mainnav-right ul
-    $menuList.fadeOut();
+    $(".mainnav-right ul").hide();
+
+    //Remove homeli and contactli
+    $("#tempHome").remove();
+    $("#tempContact").remove();
 
   } else {
     //Odd Click
 
     //Extend Menu when lines are pressed
-    $mobileNav.animate({
+    $(".nav").animate({
       height: "360px"
     }, 400);
 
     //Append homeli at beginning and contacli at the end and show
-    $menuList.fadeIn();
+    $(".mainnav-right ul").prepend(homeli).append(contactli).show();
   }
 
   $(this).data("clicks", !clicks);
